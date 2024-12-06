9ddfb6510ada4946a30872181da6db5de419b8ec
Sangeet Paudel <stiletdurden252@gmail.com>
2024-12-06 12:55:45 +0545

    Initial Commit
    This is the initial commit for the Timer Game!

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useRef } from "react";
+import ResultModal from "./ResultModal";
+
+const TimerChallenge = ({ title, targetTime }) => {
+  const timer = useRef();
+  const dialog = useRef();
+  const [timeStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  const handleStart = () => {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+      dialog.current.showModal();
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  };
+
+  const handleStop = () => {
+    clearTimeout(timer.current);
+  };
+  return (
+    <>
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <section className="challenge">
+        <h2>{title}</h2>
+        {timerExpired && <p>You Lost!</p>}
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timeStarted ? handleStop : handleStart}>
+            {timeStarted ? "Stop" : "Start"}
+          </button>
+        </p>
+        <p className={timeStarted ? "active" : undefined}>
+          {timeStarted ? "Time is running.." : "Timer inactive"}
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default TimerChallenge;
